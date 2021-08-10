ae5edc54ff9982955b6123b65bf0b5660f67b81b
Rey Mark A. Engada <reymark.engada@yahoo.com>
2021-08-10 21:09:30 +0800

    Updated added command for job scheduling on daily earnings.

---
+++ app/Console/Commands/DailyShare.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+use App\Enums\Role;
+
+class DailyShare extends Command
+{
+
+    private $bank;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'daily:earning';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate daily earnings.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->bank = User::role(Role::BANK)->first();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        // Invested tokens
+        $tokenOnInvested = 100;
+        $sharesPerMonth = .25;
+        $grossPerMonth = $tokenOnInvested*$sharesPerMonth;
+        $dailyEarnings = round($grossPerMonth/30, 2);
+
+        dd($dailyEarnings);
+
+        return ;
+    }
+}
--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -105,13 +105,14 @@
             Content body start
         ***********************************-->
         <div class="content-body">
-            <div class="alert alert-info alert-dismissible fade show">
+            <div class="alert alert-success alert-dismissible fade show">
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" class="mr-2">
                     <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="12" y1="16" x2="12" y2="12"></line>
-                    <line x1="12" y1="8" x2="12.01" y2="8"></line>
+                    <path d="M8 14s1.5 2 4 2 4-2 4-2"></path>
+                    <line x1="9" y1="9" x2="9.01" y2="9"></line>
+                    <line x1="15" y1="9" x2="15.01" y2="9"></line>
                 </svg>
-                <strong>Info!</strong> We are working on daily share earning's.
+                <strong>Info!</strong> We are working on investmenting token's .
                 <button type="button" class="close h-100" data-dismiss="alert" aria-label="Close"><span><i class="mdi mdi-close"></i></span>
                 </button>
             </div>
