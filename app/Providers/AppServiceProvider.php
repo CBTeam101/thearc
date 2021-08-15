6dd159598b56d6e328a48e0cd4dc92e4fbae5867
unknown <jeromevillaver@gmail.com>
2021-08-15 11:29:14 +0800

    Logo added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
+        Schema::defaultStringLength(125); // Spatie Recommend setup
+        // Schema::defaultStringLength(125); Laravel Recommend 
     }
 }
